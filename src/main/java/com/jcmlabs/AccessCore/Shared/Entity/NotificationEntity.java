1417ce207344b5e4364ff84d6eb700a24c0dcebd
JohnMalugu <johnmalugu99@gmail.com>
2026-01-04 16:38:48 +0300

    preparing to complete email sending

--- src/main/java/com/jcmlabs/AccessCore/Shared/Entity/NotificationEntity.java
+++ src/main/java/com/jcmlabs/AccessCore/Shared/Entity/NotificationEntity.java
@@ -1,6 +1,9 @@
 package com.jcmlabs.AccessCore.Shared.Entity;
 
+import com.jcmlabs.AccessCore.Shared.Enums.NotificationStatus;
+import com.jcmlabs.AccessCore.Shared.Enums.NotificationType;
 import com.jcmlabs.AccessCore.Utilities.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -9,6 +12,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SoftDelete;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -16,6 +21,28 @@ import org.hibernate.annotations.SoftDelete;
 @Entity
 @Table(name="notification")
 @SoftDelete()
-public class NotificationEntity extends BaseEntity {
+public class NotificationEntity extends BaseEntity implements Serializable {
+    @Column()
+    private String subject;
 
+    @Column()
+    private String emailTo;
+
+    @Column()
+    private String phoneTo;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable=false)
+    private NotificationStatus status = NotificationStatus.PENDING;
+
+    @Column(nullable = false)
+    private NotificationType notificationType;
+
+    @Column()
+    private String remarks;
 }
--- src/main/java/com/jcmlabs/AccessCore/Shared/Entity/RabbitmqMessage.java
+++ src/main/java/com/jcmlabs/AccessCore/Shared/Entity/RabbitmqMessage.java
@@ -1,4 +1,49 @@
 package com.jcmlabs.AccessCore.Shared.Entity;
 
-public class RabbitmqMessage {
+import com.jcmlabs.AccessCore.Utilities.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rabbitmq_message")
+public class RabbitmqMessage extends BaseEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "payload", columnDefinition = "TEXT", nullable = false)
+    private String payload;
+
+    @Column(name = "is_sent", columnDefinition = "BOOLEAN DEFAULT true")
+    private Boolean isSent = Boolean.TRUE;
+
+    @Column(name = "sent_time", nullable = false)
+    @JdbcTypeCode(java.sql.Types.TIMESTAMP_WITH_TIMEZONE)
+    private Instant sentTime = Instant.now();
+
+    @Column(name = "is_acknowledged", columnDefinition = "BOOLEAN DEFAULT false")
+    private Boolean isAcknowledged = Boolean.FALSE;
+
+    @Column(name = "acknowledged_time")
+    @JdbcTypeCode(java.sql.Types.TIMESTAMP_WITH_TIMEZONE)
+    private Instant acknowledgedTime;
+
+    @Column(name = "error", columnDefinition = "TEXT")
+    private String error;
+
+    @Column(name = "number_of_retries")
+    private Integer numberOfRetries = 0;
 }
