67f15739e7be9ee873445000d84372a0e598b334
JohnMalugu <johnmalugu99@gmail.com>
2026-01-06 12:45:08 +0300

    notification management with unimplemented methods

--- src/main/java/com/jcmlabs/AccessCore/Shared/Payload/Request/NotificationDto.java
+++ src/main/java/com/jcmlabs/AccessCore/Shared/Payload/Request/NotificationDto.java
@@ -1,3 +1,24 @@
 package com.jcmlabs.AccessCore.Shared.Payload.Request;
 
-public record NotificationDto() {}
+import com.jcmlabs.AccessCore.Shared.Enums.EmailType;
+import com.jcmlabs.AccessCore.Shared.Enums.NotificationStatus;
+import com.jcmlabs.AccessCore.Shared.Enums.NotificationType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record NotificationDto(
+        String uuid,
+        Long userId,
+        List<String> fcmTokens,
+        String subject,
+        String emailTo,
+        String phoneTo,
+        String fullName,
+        String message,
+        NotificationStatus status,
+        NotificationType notificationType,
+        String remarks,
+        LocalDateTime createdAt,
+        EmailType emailType
+        ) {}
