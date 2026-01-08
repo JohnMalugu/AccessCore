2023fa1c8f3f6bba5a33d11d5d656c876ab49316
JohnMalugu <johnmalugu99@gmail.com>
2026-01-08 10:03:23 +0300

    configuration files setups

--- src/main/java/com/jcmlabs/AccessCore/AccessCoreApplication.java
+++ src/main/java/com/jcmlabs/AccessCore/AccessCoreApplication.java
@@ -2,8 +2,10 @@ package com.jcmlabs.AccessCore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.jcmlabs.AccessCore")
 public class AccessCoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jcmlabs/AccessCore/Configurations/Http/HttpClientConfig.java
+++ src/main/java/com/jcmlabs/AccessCore/Configurations/Http/HttpClientConfig.java
@@ -51,6 +51,11 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class HttpClientConfig {
 
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
     /**
      * <h3>SMS Provider HTTP Client</h3>
      *
