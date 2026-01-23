6380b7a1103f7d4a4a4fee855841cd35f11de7dc
JohnMalugu <johnmalugu99@gmail.com>
2026-01-23 23:01:30 +0300

    added global exception handler

--- src/main/java/com/jcmlabs/AccessCore/Utilities/GlobalExceptionHandler.java
+++ src/main/java/com/jcmlabs/AccessCore/Utilities/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class SecurityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<?> handleBadCredentials(BadCredentialsException ex) {
