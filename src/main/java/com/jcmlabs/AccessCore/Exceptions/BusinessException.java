51543112a8dfda85d41d3734e9c3220454988a45
JohnMalugu <johnmalugu99@gmail.com>
2026-01-26 20:56:39 +0300

    added global exception handler

--- src/main/java/com/jcmlabs/AccessCore/Exceptions/BusinessException.java
+++ src/main/java/com/jcmlabs/AccessCore/Exceptions/BusinessException.java
@@ -1,4 +1,15 @@
 package com.jcmlabs.AccessCore.Exceptions;
 
-public class BusinessException {
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final Integer code;
+
+    public BusinessException(Integer code, String message) {
+        super(message);
+        this.code = code;
+    }
+
 }
