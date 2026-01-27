3fb40eec5719db81de9896d7364f1dfd259ed5e3
JohnMalugu <johnmalugu99@gmail.com>
2026-01-27 23:15:45 +0300

    exception handlers

---
+++ src/main/java/com/jcmlabs/AccessCore/Exceptions/GraphQL/GraphQLErrorCodes.java
@@ -0,0 +1,8 @@
+package com.jcmlabs.AccessCore.Exceptions.GraphQL;
+
+public final class GraphQLErrorCodes {
+    public static final String NOT_FOUND = "NOT_FOUND";
+    public static final String VALIDATION_ERROR = "VALIDATION_ERROR";
+    public static final String UNAUTHORIZED = "UNAUTHORIZED";
+    private GraphQLErrorCodes() {}
+}
