3bdd15ad1ab7babd3e8690982298aa00be242acb
JohnMalugu <johnmalugu99@gmail.com>
2026-01-27 23:14:48 +0300

    exception handlers

--- src/main/java/com/jcmlabs/AccessCore/Exceptions/GraphQL/GraphQLExceptionResolver.java
+++ src/main/java/com/jcmlabs/AccessCore/Exceptions/GraphQL/GraphQLExceptionResolver.java
@@ -1,4 +1,43 @@
 package com.jcmlabs.AccessCore.Exceptions.GraphQL;
 
-public class GraphQLExceptionResolver {
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.apache.kafka.common.errors.ResourceNotFoundException;
+import org.jspecify.annotations.NonNull;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+import graphql.ErrorType;
+
+import java.util.Map;
+
+@Component
+public class GraphQLExceptionResolver extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(
+            @NonNull Throwable ex,
+            @NonNull DataFetchingEnvironment env
+    ) {
+
+        if (ex instanceof ResourceNotFoundException rnfe) {
+            return GraphqlErrorBuilder.newError(env)
+                    .message(rnfe.getMessage())
+                    .errorType(ErrorType.DataFetchingException)
+                    .extensions(Map.of(
+                            "code", GraphQLErrorCodes.NOT_FOUND,
+                            "resource", rnfe.getResource(),
+                            "field", rnfe.getField(),
+                            "value", rnfe.getValue()
+                    ))
+                    .build();
+        }
+
+        return null;
+    }
 }
+
+
+
+
