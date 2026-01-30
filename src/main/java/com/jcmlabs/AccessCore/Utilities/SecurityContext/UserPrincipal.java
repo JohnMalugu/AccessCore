f9f878f8d966afa76fdec1fde7adb8b7f30147be
JohnMalugu <johnmalugu99@gmail.com>
2026-01-30 22:05:21 +0300

    logged in user

--- src/main/java/com/jcmlabs/AccessCore/Utilities/SecurityContext/UserPrincipal.java
+++ src/main/java/com/jcmlabs/AccessCore/Utilities/SecurityContext/UserPrincipal.java
@@ -1,4 +1,36 @@
 package com.jcmlabs.AccessCore.Utilities.SecurityContext;
 
-public class UserPrincipal {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.io.Serializable;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@Jacksonized
+public final class UserPrincipal implements Serializable {
+
+    private final Long id;
+    private final String uuid;
+    private final String username;
+    private final AccountStatus status;
+    private final Set<String> authorities;
+
+
+    public boolean isActive() {
+        return status == AccountStatus.ACTIVE;
+    }
+
+    public Set<GrantedAuthority> getAuthorities() {
+        return authorities == null
+                ? Set.of()
+                : authorities.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toUnmodifiableSet());
+    }
 }
