040846526eb283c81720509355fda2ecbcd1bedc
JohnMalugu <johnmalugu99@gmail.com>
2026-01-31 22:09:17 +0300

    redis verification token

--- src/main/java/com/jcmlabs/AccessCore/Shared/Entity/RedisVerificationToken.java
+++ src/main/java/com/jcmlabs/AccessCore/Shared/Entity/RedisVerificationToken.java
@@ -1,4 +1,28 @@
 package com.jcmlabs.AccessCore.Shared.Entity;
 
-public class RedisVerificationToken {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RedisVerificationToken implements Serializable {
+
+    private Long userId;
+
+    private String emailToken;
+
+    private String phoneOtp;
+
+    private int otpAttempts = 0;
+
+    private Instant expiresAt;
+
+    private boolean used = false;
 }
