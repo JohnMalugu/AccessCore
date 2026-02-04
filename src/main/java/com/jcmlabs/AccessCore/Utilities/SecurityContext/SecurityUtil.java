40e0c0f8a07410d61c3c10062c0d1a6faff13097
JohnMalugu <johnmalugu99@gmail.com>
2026-02-04 19:19:10 +0300

    getting logged in user from security util

--- src/main/java/com/jcmlabs/AccessCore/Utilities/SecurityContext/SecurityUtil.java
+++ src/main/java/com/jcmlabs/AccessCore/Utilities/SecurityContext/SecurityUtil.java
@@ -1,4 +1,30 @@
 package com.jcmlabs.AccessCore.Utilities.SecurityContext;
 
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class SecurityUtil {
+    private void SecurityUtils() {}
+
+    public static UserPrincipal currentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated()) {
+            throw new BadCredentialsException("Unauthenticated");
+        }
+
+        Object principal = auth.getPrincipal();
+
+        if ("anonymousUser".equals(principal)) {
+            throw new BadCredentialsException("Anonymous user is not allowed");
+        }
+
+        if (!(principal instanceof UserPrincipal userPrincipal)) {
+            String principalType = (principal == null) ? "null" : principal.getClass().getSimpleName();
+            throw new BadCredentialsException("Unsupported principal type: " + principalType);
+        }
+
+        return userPrincipal;
+    }
 }
